86932b762e6439bbf0b3483faf37cf8391467ae0
Yoyo Peng <yoyopyeah@gmail.com>
2021-11-03 15:05:06 -0400

    First file test

---
+++ src/210. Course Schedule II/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] ans = new int[numCourses];
+        for (int i = 0 ; i < prerequisites.length; i++) {
+            for (int j = 0; j < ans.length; j++) {
+                if (ans[j] == prerequisites[i][1]) {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int[] insertElement(int[] array, int index, int ele) {
+        int[] result = new int[array.length + 1];
+        boolean flag = false;
+        for (int i = 0; i < result.length; i++) {
+            if (flag) {
+                result[i] = array[i - 1];
+            } else {
+                if (i == index) {
+                    flag = true;
+                    result[i] = ele;
+                } else {
+                    result[i] = array[i];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1,2,4,5};
+        System.out.println(Arrays.toString(insertElement(test, 2, 3)));
+    }
+}
